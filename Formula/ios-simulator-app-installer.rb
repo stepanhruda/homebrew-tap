93ed9dffac23dc1c6786fc36a454041d2e949c89
Stepan Hruda <stepan.hruda@gmail.com>
2015-05-05 09:03:49 -0400

    Update ios-simulator-app-installer to 0.1.5

--- Formula/ios-simulator-app-installer.rb
+++ Formula/ios-simulator-app-installer.rb
@@ -1,8 +1,8 @@
 class IosSimulatorAppInstaller < Formula
   homepage "https://github.com/stepanhruda/ios-simulator-app-installer"
-  url "https://github.com/stepanhruda/ios-simulator-app-installer/archive/v0.1.4.tar.gz"
-  version "0.1.4"
-  sha256 "c1b419ac0f1f8005d2515ef847a7a163f3c628cdc14a36fe3ed0c951ba52dfff"
+  url "https://github.com/stepanhruda/ios-simulator-app-installer/archive/v0.1.5.tar.gz"
+  version "0.1.5"
+  sha256 "0f7c2f1b483f0203dbf54db6d533656c6f27ada1909aad7cdaed7f1f4d5c2279"
 
   depends_on :xcode => "6.3"
   depends_on :macos => :yosemite
